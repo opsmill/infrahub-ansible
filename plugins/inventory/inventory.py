8120db1ed86a333812f38919cb9ae374c78feabc
Benoit Kohler <BeArchiTek@users.noreply.github.com>
2025-03-12 12:27:42 +0100

    Update inventory.py

--- plugins/inventory/inventory.py
+++ plugins/inventory/inventory.py
@@ -308,8 +308,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
                 processor = InfrahubNodesProcessor(client=client, display=self.display)
                 self.display.v("Processing Nodes request")
                 host_node_attributes = processor.fetch_and_process(
-                    nodes=self.nodes,
-                    prefetch_relationships=self.prefetch_relationships
+                    nodes=self.nodes, prefetch_relationships=self.prefetch_relationships
                 )
             except Exception as exc:
                 raise_from(AnsibleError(str(exc)), exc)
@@ -340,7 +339,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
 
         self.api_endpoint = self.api_endpoint.strip("/")
         self.timeout = self.get_option("timeout")
-        self.prefetch_relationships = self.get_option("prefetch_relationships")
+        self.prefetch_relationships = self.get_option("prefetch_relationships", True)
         self.validate_certs = self.get_option("validate_certs", True)
         self.branch = self.get_option("branch")
         self.nodes = self.get_option("nodes")
