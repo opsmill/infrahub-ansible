034a7b4f21551c4e315537fad5c5dbdd413b5695
Mikhail Yohman <mikhail.yohman@gmail.com>
2025-08-06 17:43:19 -0600

    Fix linting/typing issue.

--- tests/unit/test_deep_update.py
+++ tests/unit/test_deep_update.py
@@ -4,7 +4,7 @@ from ansible_collections.opsmill.infrahub.plugins.module_utils.infrahub_utils im
 
 
 class TestDeepUpdate(unittest.TestCase):
-    def test_deep_update_nested(self):
+    def test_deep_update_nested(self) -> None:
         source = {"a": {"b": {"c": {"d": {"e": {"f": 1}}}}}, "z": 1}
         overrides = {"a": {"b": {"c": {"d": {"e": {"f": 2, "g": 3}, "h": 4}}, "i": 5}}, "j": 6}
         expected = {"a": {"b": {"c": {"d": {"e": {"f": 2, "g": 3}, "h": 4}}, "i": 5}}, "j": 6, "z": 1}
